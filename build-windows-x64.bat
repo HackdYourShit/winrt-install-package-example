b6298756beb353389df273e214585af6dcf479f7
Quintinity <vlad.marica1@gmail.com>
2018-06-28 12:14:10 -0700

    CMake update

--- build-windows-x64.bat
+++ build-windows-x64.bat
@@ -1,5 +1,5 @@
 @echo off
 rmdir /S /Q build
 rmdir /S /Q bin
-cmake -B.\build -H. -DCMAKE_GENERATOR_PLATFORM=x64
+cmake -B.\build -H. -DCMAKE_SYSTEM_VERSION=10.0 -DCMAKE_GENERATOR_PLATFORM=x64
 cmake --build .\build --config Release -- /p:OutDir="../bin/"
